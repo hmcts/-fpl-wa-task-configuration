d74995f5d3fa7540cc50db74b8bdc25a1ef21996
Ashley Wong <ashley.wong@justice.gov.uk>
2024-02-27 16:43:04 +0000

    Revert "DFPL-2101 (WIP)"
    This reverts commit 26d2c923807f7589a020e0b99ca8c57792344e6d.

--- src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaInitiationTest.java
@@ -137,24 +137,11 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
             Arguments.of(
                 "create-work-allocation-task",
                 Map.of(
-                    "lastCreatedWATask", "ORDER_UPLOADED",
-                    "reviewCMODecision", Map.of("urgency", "NO")
+                    "lastCreatedWATask", "ORDER_UPLOADED"
                 ),
                 Map.of(
                     "taskId", "reviewOrderCMO",
                     "name", "Review Order",
-                    "processCategories", MANAGE_OUTCOME.getValue()
-                )
-            ),
-            Arguments.of(
-                "create-work-allocation-task",
-                Map.of(
-                    "lastCreatedWATask", "ORDER_UPLOADED",
-                    "reviewCMODecision", Map.of("urgency", "YES")
-                ),
-                Map.of(
-                    "taskId", "reviewOrderCMOUrgent",
-                    "name", "Review Order (Urgent)",
                     "processCategories", MANAGE_OUTCOME.getValue()
                 )
             )
@@ -166,6 +153,6 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
         // The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         // todo - check this after evaluation period
-        assertThat(logic.getRules().size(), is(21));
+        assertThat(logic.getRules().size(), is(20));
     }
 }
